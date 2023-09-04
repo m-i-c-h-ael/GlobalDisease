93053c45ee875f0d4efccb3e164545dd579e49e9
m-i-c-h-ael <plank.michael.j@gmail.com>
2023-09-04 16:35:56 +0200

    Continue on twoLine model (2023-09-04)

--- 20230901_twoLine_model.R
+++ 20230901_twoLine_model.R
@@ -1,6 +1,10 @@
 #Find the best way to model a timecourse which is made up of two linear effects
  #the second effect starts at a given time after the first
 
+rm(list=ls())
+cat('\014')
+graphics.off()
+
 #an alternative model would be that the effect of a2 depends on level at the
  #given time point
 
@@ -8,7 +12,7 @@ set.seed(01092023)
 
 a0=2
 a1=1
-a2=-1.5
+a2= -1.5
 
 t= 0:100
 t0= 40
@@ -16,7 +20,7 @@ t0= 40
 
 y_noNoise= a0 + a1*t + a2*ifelse(t>t0,t-t0,0)
 #noise1= rnorm(length(t),mean=0,sd=1)  #constant noise level
-noise2= rnorm(length(t),mean=0,sd=0.5*y_noNoise)  #noise relative to data
+noise2= rnorm(length(t),mean=0,sd=0.1*y_noNoise)  #noise relative to data
 y= y_noNoise+noise2
 y[y<0]= 0   #make sure no negative values
 DF= cbind.data.frame(t=t,y=y)
@@ -44,33 +48,36 @@ lines(t, est[1] + est[2]*t + est[3]*ifelse(t>est[4],t-est[4],0),col='blue')
 ###******************************************************####
 #extimate parameters based on Bayesian model
 library('rjags')
-source('G:/My Drive/20230815_GlobBurdDisease/DBDA2E-utilities_mod.R')  #modified to fit markdown
+source('H:/My Drive/20230815_GlobBurdDisease/DBDA2E-utilities_mod.R')  #modified to fit markdown
 
 
 modStr= 
-'data{
+'
+data{
   #Ntotal= length(y)
    #transform data by rescaling y and shifting t to t1/2
-  t_half= (max(t)-min(t))/2
-  mean_y= mean(y)
-  sd_y= sd(y)
-  for(i in 1:Ntotal){ 
-    t_z[i]= t[i]-t_half
-    y_z[i]= (y[i]-mean_y)/sd_y 
-  }
+    t_half= (max(t)-min(t))/2
+    mean_y= mean(y)
+    sd_y= sd(y)
+    for(i in 1:Ntotal){
+      t_z[i]= t[i]-t_half
+      y_z[i]= (y[i]-mean_y)/sd_y
+    }
 }
+
 model{
   for(i in 1:Ntotal){
-    y_z[i] ~ dnorm( a0 + a1*t_z[i] + a2*(t_z[i]-t0_z), 1/sig_y^2 ) #homog. variance
+    t2[i]= ifelse(t_z[i]>t0_z,t_z[i]-t0_z,0)  #only subtracted if t_z[i]>t0_z
+    y_z[i] ~ dnorm( a0 + a1*t_z[i] + a2*t2[i], 1/sig_y^2 ) #homog. variance
   }
    #priors
-   a0 ~ dgamma(1,1)      #orig. scale mean= mean_y, ESS= 1
-   a1 ~ dgamma(1,10)     #orig. scale mean= sd_y/10, ESS= 10
+   a0 ~ dnorm(0,1)      #orig. scale mean= mean_y, ESS= 1
+   a1 ~ dgamma(.05,2)     #orig. scale mean= 0.025*sd_y, ESS= 2
    a2 = -a2_neg         #ensure a2 is negative on same scale as a1
-   a2_neg~ dgamma(1,10)
+   a2_neg~ dgamma(.05,2)
    t0_z= t0-t_half
    t0 ~ dunif(0,2*t_half)  #t0 anywhere between 0 and max(t)
-   sig_y ~ dgamma(1/10,1)
+   sig_y ~ dgamma(1/10,1) #on transformed scale
    
    #re-transform
    b0= a0*sd_y+mean_y
@@ -78,14 +85,15 @@ model{
    b2= a2*sd_y
 }'
 
+#y=c(); t=c()  #if you want priors
 dataList= list(
-  t=t,
-  y=y,
+  t= t,
+  y= y,
   Ntotal= length(y)
 )
 
 jagMod= jags.model(file=textConnection(modStr),data=dataList,n.chains=3)  
-cS= coda.samples(model=jagMod,variable.names = c('b0','b1','b2','t0','sig_y'),n.iter=10000)
+cS= coda.samples(model=jagMod,variable.names = c('b0','b1','b2','t0'),n.iter=10000)
 
 #diagnostics
 for(i in 1:length(varnames(cS))){
@@ -97,9 +105,13 @@ DF1= data.frame(cS[[1]])
 head(DF1)
 par(mfrow=c(2,2))
 hist(DF1$b0,col='skyblue',breaks=50)
+lines(x=HDIofMCMC(DF1$b0,credMass=.95),y=c(0,0),lwd=5)
 hist(DF1$b1,col='skyblue',breaks=50)
+lines(x=HDIofMCMC(DF1$b1,credMass=.95),y=c(0,0),lwd=5)
 hist(DF1$b2,col='skyblue',breaks=50)
+lines(x=HDIofMCMC(DF1$b2,credMass=.95),y=c(0,0),lwd=5)
 hist(DF1$t0,col='skyblue',breaks=50)
+lines(x=HDIofMCMC(DF1$t0,credMass=.95),y=c(0,0),lwd=5)
 
 par(mfrow=c(1,1))
 plot(DF1$b1,DF1$b2,col='skyblue')
